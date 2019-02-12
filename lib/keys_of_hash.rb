da433c33f7e74dfe62403c3ce7b96278a0c1970d
Adam Moran <jeffadammoran@gmail.com>
2019-02-12 22:27:17 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,17 @@
+require 'pry'
+
 class Hash
   def keys_of(arguments)
-    # code goes here
+    binding.pry
+    array = []
+    arguments.each do |i|
+      #Hash.each do |key, value|
+       # if i == value
+       #   array << key
+          binding.pry
+       # end
+     # end
+    end
+    return array
   end
 end
